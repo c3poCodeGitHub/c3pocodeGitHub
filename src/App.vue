75e9c548a1e213e5562a3de89768a9ae0b21a974
Mihail Pisarev <c3pogithub@gmail.com>
2021-07-06 16:23:38 +0200

    create

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  
+  }
+}
+</script>
+
+
+<style scoped>
+.header {
+  height: 50px;
+  width: 100%;
+  background-color: rgb(209, 201, 201);
+}
+</style>
